b38ce1c69dfa1ff32eae20bf9f63a5c6aa2601ee
kamtendra <skamtendra@gmail.com>
2023-10-20 23:25:39 +0530

    first commit

---
+++ job.py
@@ -0,0 +1,5 @@
+def print_number(lowest, highest):
+    print("Job processing started")
+    for i in range(lowest, highest+1):
+        print("{}\n".format(i))
+    print("Job processing completed")
